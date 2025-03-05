61ad23e830ba41f6700dbd3d148ff314ea6f42da
Mike Doswell <mike.doswell@gmail.com>
2025-03-04 17:28:46 -0800

    added MRI scanner sound and script for playing sound

---
+++ Assets/Scripts/StartScanManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro; // Required for TextMeshPro UI
+using System.Collections.Generic;
+
+public class StartScanManager : MonoBehaviour
+{
+    //public GameObject walkingPatient = null;
+    [SerializeField] private AudioSource scannerAudioSource;
+
+    private void Start()
+    {
+        // Ensure scanner sound is stopped initially
+        if (scannerAudioSource != null)
+        {
+            //scannerAudioSource.loop = true;
+            scannerAudioSource.Stop();
+        }
+    }
+    public void StartScan()
+    {
+        scannerAudioSource.Play();
+    }
+}
