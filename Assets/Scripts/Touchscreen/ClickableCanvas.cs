7985563401ba3d92a79c5ce17d8f1f18a0f28b44
tomhebcit <the18@my.bcit.ca>
2025-03-12 19:39:31 -0700

    added animation to expanded ui to make it more obvious what is happening

--- Assets/Scripts/Touchscreen/ClickableCanvas.cs
+++ Assets/Scripts/Touchscreen/ClickableCanvas.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 
 public class ClickableCanvas : MonoBehaviour, IPointerClickHandler
 {
@@ -7,18 +8,45 @@ public class ClickableCanvas : MonoBehaviour, IPointerClickHandler
     public GameObject[] closeOnOpen;
     public GameObject[] closeOnClose;
     private static bool isOpen = false;
+
+    private Vector3 expandedScale = new Vector3(5, 5, 1);
+    private Vector3 collapsedScale = new Vector3(1, 1, 1);
+    private float animationDuration = 0.3f;
+
     public void OnPointerClick(PointerEventData eventData)
     {
-        uiPanel.transform.localScale = isOpen ?new Vector3(1, 1, 1) : new Vector3(5, 5, 1);
-        setActive(closeOnOpen, isOpen);
         isOpen = !isOpen;
-        setActive(closeOnClose, isOpen);
+        StopAllCoroutines();
+        StartCoroutine(AnimateAndToggle(isOpen));
     }
+
+    private IEnumerator AnimateAndToggle(bool opening)
+    {
+        yield return StartCoroutine(ScaleOverTime(opening ? expandedScale : collapsedScale));
+        setActive(closeOnOpen, !opening);
+        setActive(closeOnClose, opening);
+    }
+
+    private IEnumerator ScaleOverTime(Vector3 targetScale)
+    {
+        float elapsedTime = 0f;
+        Vector3 startScale = uiPanel.transform.localScale;
+
+        while (elapsedTime < animationDuration)
+        {
+            uiPanel.transform.localScale = Vector3.Lerp(startScale, targetScale, elapsedTime / animationDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        uiPanel.transform.localScale = targetScale; 
+    }
+
     private void setActive(GameObject[] objects, bool active)
     {
         foreach (GameObject obj in objects)
         {
-            obj.SetActive(active);
+            if (obj != null) obj.SetActive(active);
         }
     }
 }
