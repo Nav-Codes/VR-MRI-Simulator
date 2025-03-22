e2230d6c8b4265bdaf511348db6a97a09e538166
yuyue-creater <yyu129@my.bcit.ca>
2025-03-22 13:30:31 -0700

    Basic Light Button descriptions added

--- Assets/Scripts/MachineControls/BasicLightButton.cs
+++ Assets/Scripts/MachineControls/BasicLightButton.cs
@@ -7,28 +7,42 @@ using UnityEngine.EventSystems;
 /// </summary>
 public class BasicLightButton : MonoBehaviour
 {
+    // The controller that controllers that manages light button interactions
     public ButtonLightBase buttonLightController;
+    // The light object being controlled
     public GameObject lightObject;
+    // The name of the button
     public string buttonName;
+    // Tracks the light status
     protected bool isOn = false;
-
+    
+    
+    // Initializes the button state based on the active status of the light object
     public void Start()
     {
+        // Set initial state to match the light object's active state
         isOn = lightObject.activeSelf;
     }
 
+    // Returns the state of the light
     public bool getState()
     {
         return isOn;
     }
 
+    // Turns the light on by calling the ButtonLightController
     public void TurnOn() {
+        // Turns on the light
         buttonLightController.TurnButtonOn(buttonName);
+        // Sets light status
         isOn = true;
     }
 
+    // Turns the light off by calling the ButtonLightController
     public void TurnOff() {
+        // Turns off the light
         buttonLightController.TurnButtonOff(buttonName);
+        // Sets light status
         isOn = false;
     }
 }
