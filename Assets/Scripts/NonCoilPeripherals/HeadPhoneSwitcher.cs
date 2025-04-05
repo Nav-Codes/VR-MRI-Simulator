c7e86a1d105261b59718b839f871a61e0fac4397
Mike Doswell <mike.doswell@gmail.com>
2025-04-05 10:29:58 -0700

    added headphones to error checking

--- Assets/Scripts/NonCoilPeripherals/HeadPhoneSwitcher.cs
+++ Assets/Scripts/NonCoilPeripherals/HeadPhoneSwitcher.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 /// When the headphones are snapped to a specific point (via the OnSnapped method from the ISnappable interface),
 /// it will activate the open headphone model and deactivate the closed one.
 /// </summary>
-public class HeadPhoneSwitcher : MonoBehaviour, ISnappable
+public class HeadPhoneSwitcher : MonoBehaviour, ISnappable, CheckerInterface
 {
     public GameObject headphoneClosed;
     public GameObject headphoneOpen;
+    private bool isOnPatient = false;
+
     public void OnSnapped(Transform snapPointParent)
     {
-        Debug.Log("snapPointParent: " + snapPointParent);
         if (snapPointParent != null)
         {
             headphoneOpen = snapPointParent.Find("SnapPointHeadphone").Find("Headphone_Open")?.gameObject;
@@ -21,10 +22,18 @@ public class HeadPhoneSwitcher : MonoBehaviour, ISnappable
             {
                 headphoneOpen.SetActive(true);
                 headphoneClosed.SetActive(false);
+                isOnPatient = true;
             }
         }
-
-
     }
 
+    public string getLabel()
+    {
+        return "Headphones on patient";
+    }
+
+    public bool isCorrect()
+    {
+        return isOnPatient;
+    }
 }
