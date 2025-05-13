e7678daaf655ba175ec5e3ad1743cf99624f1471
tomhebcit <the18@my.bcit.ca>
2025-05-12 19:29:56 -0700

    fixed bugged where exam could not be picked from hotspots using trigger

--- Assets/Scripts/ExamSelection/PatientHoverParticleFeedback.cs
+++ Assets/Scripts/ExamSelection/PatientHoverParticleFeedback.cs
@@ -1,50 +1,76 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Collider))]
 public class PatientHoverParticleFeedback : XRBaseInteractable
 {
     public DataBanker databanker;
     public string examType;
     public ParticleSystem hoverParticles;
+    public InputActionReference rightActivateAction;
+    public InputActionReference leftActivateAction;
+
     private int hoverCount = 0;
+    private bool isHovered = false;
 
     void Start()
     {
-        if (hoverParticles != null)
-            hoverParticles.Stop(); // Ensure the particles are not playing at the start
+        if (rightActivateAction != null)
+        {
+            rightActivateAction.action.Enable();
+            rightActivateAction.action.performed += OnActivatePressed;
+        }
+
+        if (leftActivateAction != null)
+        {
+            leftActivateAction.action.Enable();
+            leftActivateAction.action.performed += OnActivatePressed;
+        }
+    }
+
+
+    void OnDisable()
+    {
+        if (rightActivateAction != null)
+            rightActivateAction.action.performed -= OnActivatePressed;
+
+        if (leftActivateAction != null)
+            leftActivateAction.action.performed -= OnActivatePressed;
+    }
+
+
+    private void OnActivatePressed(InputAction.CallbackContext context)
+    {
+        if (isHovered)
+        {
+            databanker.SetExamType(examType);
+        }
     }
 
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
         base.OnHoverEntered(args);
-        HoverEnteredLogic();
+        isHovered = true;
+        HandleHoverEntered();
     }
 
     protected override void OnHoverExited(HoverExitEventArgs args)
     {
         base.OnHoverExited(args);
-        HoverExitedLogic();
+        isHovered = false;
+        HandleHoverExited();
     }
 
-    protected override void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        base.OnSelectEntered(args);
-        databanker.SetExamType(examType);
-    }
-
-    private void HoverEnteredLogic()
+    private void HandleHoverEntered()
     {
         hoverCount++;
-
         if (hoverParticles != null && !hoverParticles.isPlaying)
             hoverParticles.Play();
     }
 
-    private void HoverExitedLogic()
+    private void HandleHoverExited()
     {
         hoverCount = Mathf.Max(hoverCount - 1, 0);
-
         if (hoverCount == 0 && hoverParticles != null && hoverParticles.isPlaying)
             hoverParticles.Stop();
     }
