d85bb593fe0b44e88ad7406a140fb53a8199f9ba
tomhebcit <the18@my.bcit.ca>
2025-05-19 20:43:19 -0700

    fixed bug where leaving the removable plate from the bed in place passes the error check

--- Assets/Scripts/SnapPoint/BottomCoilStatus.cs
+++ Assets/Scripts/SnapPoint/BottomCoilStatus.cs
@@ -6,7 +6,7 @@ public class BottomCoilStatus : MonoBehaviour, CheckerInterface
 {
     public bool isCorrect()
     {
-        return transform.childCount > 0;
+        return gameObject.CompareTag("Coil");
     }
 
     public string getLabel()
