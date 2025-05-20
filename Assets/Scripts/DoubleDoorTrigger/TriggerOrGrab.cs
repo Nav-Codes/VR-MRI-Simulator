942083762a5c54d3f03a1446d9e3ba89f12fbcaa
tomhebcit <the18@my.bcit.ca>
2025-05-20 11:13:49 -0700

    fixed bug where one controller is pointed at each handle of double doors, the door opens without animation then immediately animates closed

--- Assets/Scripts/DoubleDoorTrigger/TriggerOrGrab.cs
+++ Assets/Scripts/DoubleDoorTrigger/TriggerOrGrab.cs
@@ -3,43 +3,49 @@ using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.InputSystem;
 using UnityEngine.Events;
 
-
 public class TriggerOrGrab : XRBaseInteractable
 {
     public InputActionReference rightActivateAction;
     public InputActionReference leftActivateAction;
     public UnityEvent TriggerOrGrabEvent;
-    private bool isHovered = false;
+
+    private XRBaseControllerInteractor hoveringInteractor = null;
 
     void Start()
     {
         if (rightActivateAction != null)
         {
             rightActivateAction.action.Enable();
-            rightActivateAction.action.performed += OnActivatePressed;
+            rightActivateAction.action.performed += OnRightActivatePressed;
         }
 
         if (leftActivateAction != null)
         {
             leftActivateAction.action.Enable();
-            leftActivateAction.action.performed += OnActivatePressed;
+            leftActivateAction.action.performed += OnLeftActivatePressed;
         }
     }
-
 
     void OnDisable()
     {
         if (rightActivateAction != null)
-            rightActivateAction.action.performed -= OnActivatePressed;
+            rightActivateAction.action.performed -= OnRightActivatePressed;
 
         if (leftActivateAction != null)
-            leftActivateAction.action.performed -= OnActivatePressed;
+            leftActivateAction.action.performed -= OnLeftActivatePressed;
     }
 
-
-    private void OnActivatePressed(InputAction.CallbackContext context)
+    private void OnRightActivatePressed(InputAction.CallbackContext context)
     {
-        if (isHovered)
+        if (hoveringInteractor != null && hoveringInteractor.gameObject.CompareTag("RightController"))
+        {
+            TriggerOrGrabEvent.Invoke();
+        }
+    }
+
+    private void OnLeftActivatePressed(InputAction.CallbackContext context)
+    {
+        if (hoveringInteractor != null && hoveringInteractor.gameObject.CompareTag("LeftController"))
         {
             TriggerOrGrabEvent.Invoke();
         }
@@ -48,14 +54,23 @@ public class TriggerOrGrab : XRBaseInteractable
     protected override void OnHoverEntered(HoverEnterEventArgs args)
     {
         base.OnHoverEntered(args);
-        isHovered = true;
+
+        if (args.interactorObject is XRBaseControllerInteractor controller)
+        {
+            hoveringInteractor = controller;
+        }
     }
 
     protected override void OnHoverExited(HoverExitEventArgs args)
     {
         base.OnHoverExited(args);
-        isHovered = false;
+
+        if (args.interactorObject == hoveringInteractor)
+        {
+            hoveringInteractor = null;
+        }
     }
+
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
